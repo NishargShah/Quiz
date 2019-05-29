0408051f4b38ae8572fb2ee0016dcfd68a125eeb
NishargShah <nishargshah3101@gmail.com>
2019-05-30 03:45:27 +0530

    add es6 version format

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var UIController = (function () {
+let UIController = (() => {
 	//  ALL CLASSES THAT ARE USED IN THIS PROJECT
-	var DOMData = {
+	const DOMData = {
 		op1: '.op1', op2: '.op2', op3: '.op3', op4: '.op4', op5: '.op5',
 		next: '.next', entry: '.entry', form: 'form', point: '.point',
 		question: '.question', option: '.option', congo: '.congo',
@@ -10,7 +10,7 @@ var UIController = (function () {
 	};
 
 	// ALL QUESTIONS
-	var questions = {
+    const questions = {
 		que1: "Nisharg Site Name?",
 		que2: "How Many Subscribers Are Nisharg Have In His Youtube Channel?",
 		que3: "How Many Programming Languages Nisharg Knows Perfectly?",
@@ -19,7 +19,7 @@ var UIController = (function () {
 	};
 
 	// ALL OPTIONS
-	var options = {
+    const options = {
 		op1: ['https://nisharg.com', 'https://nisarg.me', 'https://nisharg.tech', 'https://nisharg.me'],
 		op2: ['>=100', '>=250', '>=500', '>=1000'],
 		op3: [3, 5, 7, 9],
@@ -28,7 +28,7 @@ var UIController = (function () {
 	};
 
 	// ALL ANSWER
-    var answers = {
+    const answers = {
         ans1: [0, 3],
         ans2: [1, 1],
         ans3: [2, 2],
@@ -38,33 +38,25 @@ var UIController = (function () {
 
 	// RETURN THE FUNCTION OF UIController
 	return {
-		getDom: function () {
-			return DOMData
-		},
-		getQuestions: function () {
-			return questions
-		},
-		getOptions: function () {
-			return options
-		},
-        getAnswers: function () {
-            return answers
-        }
+	    getDom: () => DOMData,
+        getQuestions: () => questions,
+        getOptions: () => options,
+        getAnswers: () => answers
 	}
 })();
 
-var updateController = (function (ui) {
+let updateController = (ui => {
 	// ACCESS UIController DOMs VARIABLE
-    var updateDom, clicked, getQuestions, getOptions;
+    let updateDom, clicked, getQuestions, getOptions;
 	updateDom = ui.getDom();
 	clicked = updateDom.clicked;
     getQuestions = ui.getQuestions();
     getOptions = ui.getOptions();
 
 	// ADD DATA INTO UI
-	var addData =  function(addQuestion, addOptions, once) {
+	let addData =  (addQuestion, addOptions, once) => {
 	    // ADD HTML DYNAMICALLY WHEN EVERY CALL
-		var html =
+		let html =
 			'<p class="question">' + addQuestion + '</p><br>' +
 			'<div class="option op1">' + addOptions[0] +'</div>' +
 			'<div class="option op2">' + addOptions[1] +'</div>' +
@@ -76,27 +68,27 @@ var updateController = (function (ui) {
 			document.querySelector(updateDom.container).insertAdjacentHTML('beforeend', html);
 		} else {
 			document.querySelector(updateDom.container + ' p').innerHTML = addQuestion;
-			for (var i = 1; i <= 4; i++) {
+			for (let i = 1; i <= 4; i++) {
 				document.querySelector(updateDom.container + ' .op' + i).innerHTML = addOptions[i - 1];
 			}
 		}
 	};
 
 	// PROVIDE DATA WHEN YOU CLICK ON NEXT BUTTON
-	var clickData = function () {
+	let clickData = () => {
         // THIS FUNCTION FIRED INITIALLY
-        var initiallyData = function () {
+        let initiallyData = () => {
             addData(getQuestions.que1, getOptions.op1, true);
         };
 
         // REPLACE DATA WHEN NEXT BUTTON PRESSED
-        var updateData = function() {
-            for (var i = 0; i <= 3; i++) {
+        let updateData = () => {
+            for (let i = 0; i <= 3; i++) {
                 if (clicked === i) {
-                    var concatIQuestion = 'que' + (i + 2);
-                    var concatIOption = 'op' + (i + 2);
-                    var loopedQuestion = getQuestions[concatIQuestion];
-                    var loopedOption = getOptions[concatIOption];
+                    let concatIQuestion = 'que' + (i + 2);
+                    let concatIOption = 'op' + (i + 2);
+                    let loopedQuestion = getQuestions[concatIQuestion];
+                    let loopedOption = getOptions[concatIOption];
                     addData(loopedQuestion, loopedOption, false);
                 }
             }
@@ -110,53 +102,46 @@ var updateController = (function (ui) {
         };
 
         // INCREASE PERCENTAGE IN UI BY 20
-		var increasePercentage = function () {
-			var width = document.querySelector(updateDom.progressBar).style.width.split('%');
+        let increasePercentage = () => {
+            let width = document.querySelector(updateDom.progressBar).style.width.split('%');
 			width = parseInt(width[0]) + 20;
 			document.querySelector(updateDom.progressBar).style.width = width + '%';
 		};
 
         // INCREASE POINT IN UI BY 1
-		var increasePoint = function () {
-			var pointValue = document.querySelector(updateDom.point).innerHTML.split('/');
+		let increasePoint = () => {
+			let pointValue = document.querySelector(updateDom.point).innerHTML.split('/');
 			pointValue = parseInt(pointValue[0]) + 1;
 			document.querySelector(updateDom.point).innerHTML = pointValue + '/5';
 		};
 
 		// RETURN OF clickData
 		return {
-		    getInitiallyData: function () {
-		        return initiallyData();
-            },
-            getUpdateData: function () {
-                return updateData();
-            },
-            getIncreasePercentage: function () {
-                return increasePercentage();
-            },
-            getIncreasePoint: function () {
-                return increasePoint();
-            }
+            getInitiallyData: () => initiallyData(),
+            getUpdateData: () => updateData(),
+            getIncreasePercentage: () => increasePercentage(),
+            getIncreasePoint: () => increasePoint()
         }
 	};
 
 	// WHEN YOU CLICK ON OPTION THIS FUNCTION IS FIRED
-    var clickOnOption = function () {
-        var optionVar = document.querySelectorAll(updateDom.option);
-        var answers = ui.getAnswers();
+    let clickOnOption = () => {
+        let optionVar = document.querySelectorAll(updateDom.option);
+        let answers = ui.getAnswers();
 
         // WHEN YOU CLICK ON OPTION THIS EVENT IS FIRED
-        for (var i = 0; i < optionVar.length; i++) {
-            optionVar[i].addEventListener('click', function (current) {
+        for (let i = 0; i < optionVar.length; i++) {
+            optionVar[i].addEventListener('click', current => {
                 // CURRENT OPTION WHEN YOU CLICK
-                var currentLength = current.target.classList[1];
-                var currentVar = (currentLength.substr(currentLength.length - 1)) - 1;
+                let currentLength, currentVar, concatIAnswer, correctAnswer;
+                currentLength = current.target.classList[1];
+                currentVar = (currentLength.substr(currentLength.length - 1)) - 1;
 
                 // GET CORRECT ANSWER
-                for (var i = 0; i <= 4; i++) {
+                for (let i = 0; i <= 4; i++) {
                     if (clicked === i) {
-                        var concatIAnswer = 'ans' + (i + 1);
-                        var correctAnswer = answers[concatIAnswer][1];
+                        concatIAnswer = 'ans' + (i + 1);
+                        correctAnswer = answers[concatIAnswer][1];
                     }
                 }
 
@@ -182,26 +167,26 @@ var updateController = (function (ui) {
     };
 
     // UPDATE SCORE EVERY TIME WHEN YOU CLICK ON NEXT BUTTON
-    var updateScore = function () {
+    let updateScore = () => {
         document.querySelector(updateDom.change).innerHTML = updateDom.score;
     };
 
     // SET COLOR WHITE WHEN CLICK ON NEXT
-    var setBG2White = function () {
-        var optionVar = document.querySelectorAll(updateDom.option);
-        for (var i = 0; i < 4; i++) {
+    let setBG2White = () => {
+        let optionVar = document.querySelectorAll(updateDom.option);
+        for (let i = 0; i < 4; i++) {
             optionVar[i].style.backgroundColor = "white";
             optionVar[i].style.color = "black";
         }
     };
 
     // WHEN YOU CLICK NEXT BUTTON THIS FUNCTION FIRED
-	var clickOnNext = function () {
+    let clickOnNext = () => {
         // THIS EVENT FIRED INITIALLY
         clickData().getInitiallyData();
 
         // WHEN YOU CLICK NEXT BUTTON THIS EVENT FIRED
-        document.querySelector(updateDom.next).addEventListener('click', function () {
+        document.querySelector(updateDom.next).addEventListener('click', () => {
             clickData().getUpdateData();
             clickData().getIncreasePercentage();
             clickData().getIncreasePoint();
@@ -212,33 +197,29 @@ var updateController = (function (ui) {
 
     // RETURN THE FUNCTION OF updateController
 	return {
-        getClickOnNext: function () {
-            return clickOnNext();
-        },
-        getClickOnOption: function () {
-            return clickOnOption()
-        }
+        getClickOnNext: () => clickOnNext(),
+        getClickOnOption: () => clickOnOption()
 	}
 })(UIController);
 
-var controller = (function (ui, update) {
+let controller = ((ui, update) => {
 	// ACCESS UIController DOMs VARIABLE
-	var controllerDOM = ui.getDom();
+	const controllerDOM = ui.getDom();
 
 	// ALL EVENT HANDED BY THIS FUNCTION
-	var eventHandler = function () {
-		document.querySelector(controllerDOM.form).addEventListener('submit', function () {
+	let eventHandler = () => {
+		document.querySelector(controllerDOM.form).addEventListener('submit', () => {
 			document.querySelector(controllerDOM.containerFluid).style.display = 'block';
 			document.querySelector(controllerDOM.next).style.display = 'block';
 			document.querySelector(controllerDOM.entry).style.display = 'none';
 			const name = document.querySelector(controllerDOM.formControl).value;
-			alert("Welcome " + name);
+			alert(`Welcome ${name}`);
 		});
 	};
 
     // RETURN THE FUNCTION OF controller
 	return {
-		init: function () {
+		init: () => {
 			eventHandler();
 			update.getClickOnNext();
 			update.getClickOnOption();
